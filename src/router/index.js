d524d49192fa3c0e41b7bf24ba01b809548673a7
Little-monster <1580300779.qq.com>
2021-05-14 21:41:28 +0800

    项目创建

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  
+]
+
+const router = new router({
+  routes
+})
+
+export default router
---
+++ views/corps/Corps.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>团队管理</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Corps'
+  }
+
+</script>
+<style scoped>
+
+</style>
