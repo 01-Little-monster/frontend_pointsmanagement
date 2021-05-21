2ad4c821e7866e41265b55bfba9a7d24757c1b91
Little-monster <1580300779.qq.com>
2021-05-21 22:58:50 +0800

    profile—module MainAvatarUnit 修改

--- src/components/content/mainAvatarUnit/mainAvatarUnit.vue
+++ src/components/content/mainAvatarUnit/mainAvatarUnit.vue
@@ -36,7 +36,7 @@ import AvatarUnitItemInfo from '@/components/common/AvatarUnit/AvatarUnitItemInf
     },
     data () {
       return {
-        circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+        circleUrl: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
       }
     }
   }
